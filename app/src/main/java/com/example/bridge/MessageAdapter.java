bed989964fc75b6680215f79df63fef4590eb23b
Karen Wong <kxrenwong12@gmail.com>
2020-02-15 14:37:19 -0800

    karen's update! (main activity update and new Message class)

---
+++ app/src/main/java/com/example/bridge/MessageAdapter.java
@@ -0,0 +1,31 @@
+package com.example.bridge;
+import android.widget.BaseAdapter;
+
+public class MessageAdapter extends BaseAdapter {
+    List<Message> messages = new ArrayList<Message>();
+    Context context;
+
+    public MessageAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void add(Message message) {
+        this.messages.add(message);
+        notifyDataSetChanged(); // to render the list we need to notify
+    }
+
+    @Override
+    public int getCount() {
+        return messages.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return messages.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+}
