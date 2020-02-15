b86ccdd05db8ea521d5562f27931c61b32502384
Karen Wong <kxrenwong12@gmail.com>
2020-02-15 12:41:20 -0800

    git commit -m git push -u origin master

--- app/src/main/java/com/example/bridge/MainActivity.java
+++ app/src/main/java/com/example/bridge/MainActivity.java
@@ -1,15 +1,22 @@
-package com.example.bridge;
+<?xml version="1.0" encoding="utf-8"?>
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+        package="com.example.bridgeapp">
+<uses-permission android:name="android.permission.INTERNET" />
 
-import androidx.appcompat.app.AppCompatActivity;
+<application
+        android:allowBackup="true"
+                android:icon="@mipmap/ic_launcher"
+                android:label="@string/app_name"
+                android:roundIcon="@mipmap/ic_launcher_round"
+                android:supportsRtl="true"
+                android:theme="@style/AppTheme">
+<activity android:name=".MainActivity">
+<intent-filter>
+<action android:name="android.intent.action.MAIN" />
 
-import android.os.Bundle;
+<category android:name="android.intent.category.LAUNCHER" />
+</intent-filter>
+</activity>
+</application>
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-}
+</manifest>
