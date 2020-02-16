4564a744bef44bfd6cdd710e94e9090e349d8040
Gabrielle Lawrencia <glawrenc@cs.washington.edu>
2020-02-16 10:45:50 -0800

    update #2 G

---
+++ app/src/main/java/com/example/bridge/OptionPageUI.java
@@ -0,0 +1,14 @@
+package com.example.bridge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OptionPageUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option_page_ui);
+    }
+}
--- app/src/main/java/com/example/bridge/SettingsPageUI.java
+++ app/src/main/java/com/example/bridge/SettingsPageUI.java
@@ -12,6 +12,7 @@ public class SettingsPageUI extends AppCompatActivity {
     Button btnAccount;
     Button btnContacts;
     Button btnMessages;
+    Button btnLanguages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +22,7 @@ public class SettingsPageUI extends AppCompatActivity {
         btnAccount = findViewById(R.id.accountButton);
         btnContacts = findViewById(R.id.contactsButton);
         btnMessages = findViewById(R.id.messagesButton);
+        btnLanguages = findViewById(R.id.languagesButton);
 
         btnContacts.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +46,15 @@ public class SettingsPageUI extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 Intent launchLogOut = new Intent(getApplicationContext(), AccountPageUI.class);
+                startActivity(launchLogOut);
+                finish();
+            }
+        });
+
+        btnLanguages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent launchLogOut = new Intent(getApplicationContext(), LanguagePageUI.class);
                 startActivity(launchLogOut);
                 finish();
             }
