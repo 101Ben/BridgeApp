b5a945f7cf9d40ce3e2848e1e04a8ef4e30976d9
andreacallista <andreacallista17@gmail.com>
2020-02-16 15:01:39 -0800

    Andrea's edit

--- app/src/main/java/com/example/bridge/WelcomingPageUI.java
+++ app/src/main/java/com/example/bridge/WelcomingPageUI.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -20,6 +21,7 @@ import java.util.Map;
 public class WelcomingPageUI extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     FirebaseAuth mFirebaseAuth;
+    Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,21 +29,32 @@ public class WelcomingPageUI extends AppCompatActivity implements AdapterView.On
         setContentView(R.layout.activity_welcoming_page_ui);
 
         mFirebaseAuth = FirebaseAuth.getInstance();
+        btnContinue = findViewById(R.id.button);
         Spinner spinner =findViewById(R.id.languagesWPButton);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languages_list, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        btnContinue.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent i = new Intent(WelcomingPageUI.this, HomePageUI.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         String text = adapterView.getItemAtPosition(i).toString();
         String userID = mFirebaseAuth.getCurrentUser().getUid();
         DatabaseReference currentUser = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
-        Map prefLang = new HashMap();
-        prefLang.put("language", text);
-        currentUser.setValue(prefLang);
+        Register.newPost.put("language", text);
+        currentUser.setValue(Register.newPost);
 
         Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
         //startActivity(new Intent(WelcomingPageUI.this, HomePageUI.class));
@@ -52,4 +65,5 @@ public class WelcomingPageUI extends AppCompatActivity implements AdapterView.On
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
 }
