f15b94fe8824a6ffd22d3ce30cddf001d5b35f50
Gabrielle Lawrencia <glawrenc@cs.washington.edu>
2020-02-16 15:04:43 -0800

    Merge branch 'master' of /Users/gabriellelawrencia/AndroidStudioProjects/BridgeApp with conflicts.

--- app/src/main/java/com/example/bridge/LanguagePageUI.java
+++ app/src/main/java/com/example/bridge/LanguagePageUI.java
@@ -12,20 +12,42 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class LanguagePageUI extends AppCompatActivity {
+public class LanguagePageUI extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language_page_ui);
+        btnBack = findViewById(R.id.goBackButton);
 
-        Spinner spinner = (Spinner) findViewById(R.id.languagesList);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.languages_list, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
+        Spinner spinner = findViewById(R.id.languagesSpinnerButton);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languages_list, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent launchLogOut = new Intent(getApplicationContext(), SettingsPageUI.class);
+                startActivity(launchLogOut);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
--- app/src/main/java/com/example/bridge/WelcomingPageUI.java
+++ app/src/main/java/com/example/bridge/WelcomingPageUI.java
@@ -10,24 +10,14 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class WelcomingPageUI extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
-    FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcoming_page_ui);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        Spinner spinner =findViewById(R.id.languagesWPButton);
+        Spinner spinner = findViewById(R.id.languagesWPButton);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.languages_list, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
@@ -37,12 +27,6 @@ public class WelcomingPageUI extends AppCompatActivity implements AdapterView.On
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
         String text = adapterView.getItemAtPosition(i).toString();
-        String userID = mFirebaseAuth.getCurrentUser().getUid();
-        DatabaseReference currentUser = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
-        Map prefLang = new HashMap();
-        prefLang.put("language", text);
-        currentUser.setValue(prefLang);
-
         Toast.makeText(adapterView.getContext(), text, Toast.LENGTH_SHORT).show();
         //startActivity(new Intent(WelcomingPageUI.this, HomePageUI.class));
 
