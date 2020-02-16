64abf18fc408a99491d3ef26552f614e10ffc3f7
Karen Wong <kxrenwong12@gmail.com>
2020-02-16 15:24:29 -0800

    karen's changes!

--- app/src/main/java/com/example/bridge/Message.java
+++ app/src/main/java/com/example/bridge/Message.java
@@ -1,28 +1,110 @@
-
 package com.example.bridge;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Message extends User {
-    private String text;
-    private User userData;
-    private boolean belongToCurrentUser;
+import androidx.appcompat.app.AppCompatActivity;
 
-    public Message(String text, User userData, boolean belongToCurrentUser) {
-        this.text = text;
-        this.userData = userData;
-        this.belongToCurrentUser = belongToCurrentUser;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Date;
+
+public class Message extends AppCompatActivity {
+
+    private String messageText;
+    private String messageUser;
+    private long messageTime;
+
+    public Message(String messageText, String messageUser) {
+        this.messageText = messageText;
+        this.messageUser = messageUser;
+
+        messageTime = new Date().getTime();
     }
 
-    public String getText() {
-        return text;
+    public Message(){
+
     }
 
-    public User getUserData() {
-        return userData;
+    public String getMessageText() {
+        return messageText;
     }
 
-    public boolean getBelongToCurrentUser() {
-        return belongToCurrentUser;
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
     }
+
+    public String getMessageUser() {
+        return messageUser;
+    }
+
+    public void setMessageUser(String messageUser) {
+        this.messageUser = messageUser;
+    }
+
+    public long getMessageTime() {
+        return messageTime;
+    }
+
+    public void setMessageTime(long messageTime) {
+        this.messageTime = messageTime;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Button fab = findViewById(R.id.buttonsend);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = findViewById(R.id.sendMessage);
+
+                FirebaseDatabase.getInstance()
+                        .getReference()
+                        .push()
+                        .setValue(new Message(input.getText().toString(),
+                                FirebaseAuth.getInstance()
+                                        .getCurrentUser()
+                                        .getDisplayName())
+                        );
+
+                input.setText("");
+            }
+        });
+    }
+
 }
 
-
+//
+//package com.example.bridge;
+//
+//public class Message extends User {
+//    private String text;
+//    private User userData;
+//    private boolean belongToCurrentUser;
+//
+//    public Message(String text, User userData, boolean belongToCurrentUser) {
+//        this.text = text;
+//        this.userData = userData;
+//        this.belongToCurrentUser = belongToCurrentUser;
+//    }
+//
+//    public String getText() {
+//        return text;
+//    }
+//
+//    public User getUserData() {
+//        return userData;
+//    }
+//
+//    public boolean getBelongToCurrentUser() {
+//        return belongToCurrentUser;
+//    }
+//}
+//
+//
