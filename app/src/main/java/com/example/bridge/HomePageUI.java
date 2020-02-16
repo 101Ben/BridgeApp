e7db474304baf445bd4054ef2db49500e7b4f0a9
andreacallista <andreacallista17@gmail.com>
2020-02-15 19:39:40 -0800

    Andrea's edit

--- app/src/main/java/com/example/bridge/HomePageUI.java
+++ app/src/main/java/com/example/bridge/HomePageUI.java
@@ -2,13 +2,31 @@ package com.example.bridge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePageUI extends AppCompatActivity {
+
+    Button btnAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page_ui);
+
+        btnAccount = findViewById(R.id.account);
+
+        btnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent launchAccount = new Intent(getApplicationContext(), AcountPageUI.class);
+                startActivity(launchAccount);
+                finish();
+            }
+        });
     }
+
 }
